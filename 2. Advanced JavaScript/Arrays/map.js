7a147e342bf7236231902c0570d77e7a9616f708
Bartosz Legięć <bartosz+a.github@legiec.io>
2021-09-09 16:58:52 +0200

    map.js

---
+++ 2. Advanced JavaScript/Arrays/map.js
@@ -0,0 +1,7 @@
+const cats = ['Molly', 'Flower', 'Kitty']
+const grettings = cats.map(cat => `Hello ${cat}`)
+const goodbyes = cats.map(cat => `Goodbye ${cat}`)
+
+// cats: ['Molly', 'Flower', 'Kitty']
+// greetings: ['Hello Molly', 'Hello Flower', 'Hello Kitty']
+// goodbyes: ['Hello Molly', 'Hello Flower', 'Hello Kitty']
